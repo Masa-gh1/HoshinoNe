502b28e2a5653fc5f86a083d0087c60953e96520
Masakazu Inoue <minoue@highwell.jp>
2025-12-19 15:51:30 +0900

    2025-12-08

---
+++ version.py
@@ -0,0 +1,13 @@
+'''
+Version for FlowEditor
+
+Copyright (c) 2025 Masakazu Inoue
+All rights reserved.
+
+@author: Masakazu Inoue
+'''
+
+from _version import VERSION_DATE
+
+# varsion
+VERSION = f"0.1.{VERSION_DATE}"
