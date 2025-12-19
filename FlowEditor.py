ec54283860a47a8034cdf955a44a71c0fa0aa275
Masakazu Inoue <minoue@highwell.jp>
2025-12-19 12:46:40 +0900

    2025-11-16

---
+++ FlowEditor.py
@@ -0,0 +1,17 @@
+'''
+Flow Editor - Visual Flow-based Image Processing Tool
+
+Copyright (c) 2025 Masakazu Inoue
+All rights reserved.
+
+Created on 2025/10/21
+@author: Masakazu Inoue
+'''
+
+import tkinter as tk
+from main.FlowEditor import FlowEditor
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = FlowEditor(root)
+    root.mainloop()
