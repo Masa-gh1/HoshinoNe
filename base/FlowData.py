64e0eafa1290d301d8e11e6a16347a5cf02eb6dc
Masakazu Inoue <minoue@highwell.jp>
2026-01-20 13:14:30 +0900

    コード整理

--- base/FlowData.py
+++ base/FlowData.py
@@ -157,11 +157,6 @@ class FlowData:
             arr = dataBlock.data
         elif dataBlock.data.dtype != nh.BDTYPE:
             arr = dataBlock.data.astype(nh.BDTYPE)
-        elif (   not dataBlock.data.base is None
-             and     dataBlock.data.nbytes < dataBlock.data.base.nbytes
-             ):
-            # numpy 配列が view で実体がより大きいので copy して実体にする。(元の実体を開放する為)
-            arr = dataBlock.data.copy()
         else:
             arr = dataBlock.data
         
