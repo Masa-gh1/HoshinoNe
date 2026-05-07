a26399531d24feca0ddb3e53a4a1f6168931c63c
Masakazu Inoue <minoue@highwell.jp>
2026-05-07 16:06:18 +0900

    ノルムの二乗を追加

--- nodes/preset/SquaredNormNode.py
+++ nodes/preset/SquaredNormNode.py
@@ -1,5 +1,5 @@
 '''
-AbsoluteNode - 絶対値ノード
+SquaredNormNode - ノルムの二乗ノード
 
 Copyright (c) 2025 Masakazu Inoue
 All rights reserved.
@@ -11,12 +11,12 @@ from base.FlowNode_CONST import *
 from base import LazyFlowData
 from nodes import LazyNNOperationNode 
 
-class AbsoluteNode(LazyNNOperationNode):
+class SquaredNormNode(LazyNNOperationNode):
     # ノードタイプ
-    majorType = _MAJOR_TYPE_U_OP
-    minorType = 'absolute'
+    majorType = _MAJOR_TYPE_FUNC
+    minorType = 'squared_norm'
     # ノード名
-    name      = '絶対値'
+    name      = 'ノルムの二乗'
     # 入出力タイプ
     #ioType    = スーパークラスを継承
     #outputCat = スーパークラスを継承
@@ -33,6 +33,17 @@ class AbsoluteLazyFlowData(LazyFlowData):
         import numpy as np
         from base import DataBlock
         
-        result = np.abs(block.data)
+        if np.iscomplexobj(block.data):
+            # 複素数なので、二乗して加算
+            #    | a   + bi         |^2
+            #  = ((a^2 + b ^2)^(1/2))^2 複素数の大きさ(ノルム)を三平方の定理で求める
+            #  =   a^2 + b ^2
+            result = np.square(block.data.real) + np.square(block.data.imag)
+        elif np.issubdtype(block.data.dtype, np.number):
+            # 実数なので、二乗する
+            result = np.square(block.data)
+        else:
+            # その他なので、実直に計算する
+            result = np.square(np.abs(block.data))
         
         return DataBlock(result, planeIndex, x, y)
