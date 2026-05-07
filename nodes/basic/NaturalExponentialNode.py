4dc72e9c41be6f23257dfe0549db54a2eae1b638
Masakazu Inoue <minoue@highwell.jp>
2026-05-07 18:52:39 +0900

    自然指数 と 自然対数 と追加、ガウシアンフローを追加

---
+++ nodes/basic/NaturalExponentialNode.py
@@ -0,0 +1,38 @@
+'''
+NaturalExponentialNode - 自然指数ノード
+
+Copyright (c) 2025 Masakazu Inoue
+All rights reserved.
+
+@author: Masakazu Inoue
+'''
+
+from base.FlowNode_CONST import *
+from base import LazyFlowData
+from nodes import LazyNNOperationNode 
+
+class NaturalExponentialNode(LazyNNOperationNode):
+    # ノードタイプ
+    majorType = _MAJOR_TYPE_U_OP
+    minorType = 'natural_exponential'
+    # ノード名
+    name      = '自然指数'
+    # 入出力タイプ
+    #ioType    = スーパークラスを継承
+    #outputCat = スーパークラスを継承
+
+    def __init__(self, canvas, editor, x, y, **kwargs):
+        super().__init__(canvas, editor, x, y, **kwargs)
+    
+    def createLazyFlowData(self, inputData):
+        """LazyFlowDataを作成"""
+        return AbsoluteLazyFlowData(inputData)
+
+class AbsoluteLazyFlowData(LazyFlowData):
+    def blockOperation(self, block, planeIndex, x, y):
+        import numpy as np
+        from base import DataBlock
+        
+        result = np.exp(block.data)
+        
+        return DataBlock(result, planeIndex, x, y)
---
+++ nodes/basic/NaturalLogarithmNode.py
@@ -0,0 +1,38 @@
+'''
+NaturalLogarithmNode - 自然対数ノード
+
+Copyright (c) 2025 Masakazu Inoue
+All rights reserved.
+
+@author: Masakazu Inoue
+'''
+
+from base.FlowNode_CONST import *
+from base import LazyFlowData
+from nodes import LazyNNOperationNode 
+
+class NaturalLogarithmNode(LazyNNOperationNode):
+    # ノードタイプ
+    majorType = _MAJOR_TYPE_U_OP
+    minorType = 'natural_logarithm'
+    # ノード名
+    name      = '自然対数'
+    # 入出力タイプ
+    #ioType    = スーパークラスを継承
+    #outputCat = スーパークラスを継承
+
+    def __init__(self, canvas, editor, x, y, **kwargs):
+        super().__init__(canvas, editor, x, y, **kwargs)
+    
+    def createLazyFlowData(self, inputData):
+        """LazyFlowDataを作成"""
+        return AbsoluteLazyFlowData(inputData)
+
+class AbsoluteLazyFlowData(LazyFlowData):
+    def blockOperation(self, block, planeIndex, x, y):
+        import numpy as np
+        from base import DataBlock
+        
+        result = np.log(block.data)
+        
+        return DataBlock(result, planeIndex, x, y)
