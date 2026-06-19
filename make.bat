d744e2d7d47fefcbfb8eb48b5e199960297ba7f8
Masakazu Inoue <minoue@highwell.jp>
2026-06-19 18:21:20 +0900

    コード整理

--- make.bat
+++ make.bat
@@ -8,11 +8,12 @@ pyinstaller HoshinoNe.spec
 
 rename dist\HoshinoNe.exe HoshinoNe_%VERSION_DATE%.exe
 
-pip-licenses --with-license-file --no-license-path --output-file=dist\THIRD-PARTY-LICENSES
+pip-licenses --with-license-file --no-license-path --output-file=THIRD-PARTY-LICENSES
 
-copy config.py        dist\
-copy LICENSE          dist\
-copy requirements.txt dist\
-copy README.md        dist\
+copy config.py            dist\
+copy requirements.txt     dist\
+copy THIRD-PARTY-LICENSES dist\
+copy LICENSE              dist\
+copy README.md            dist\
 
 pause
